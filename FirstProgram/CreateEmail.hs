3cba01ec074fc90960c5ba2baee9cab987b3b567
Hammy <sgoudham@gmail.com>
2021-04-04 17:41:15 +0100

    Create First Program

---
+++ FirstProgram/CreateEmail.hs
@@ -0,0 +1,23 @@
+toPart :: [Char] -> [Char]
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+
+bodyPart :: [Char] -> [Char]
+bodyPart bookTitle = "Thanks for Purchasing" ++ bookTitle ++ ".\n"
+
+fromPart :: [Char] -> [Char]
+fromPart author = "Thanks, \n" ++ author
+
+createEmail :: [Char] -> [Char] -> [Char] -> [Char]
+createEmail recipient bookTitle author = toPart recipient ++ 
+                                         bodyPart bookTitle ++ 
+                                         fromPart author
+
+main :: IO ()
+main = do
+   print "Who is the email for?"
+   recipient <- getLine
+   print "What is the Title?"
+   title <- getLine
+   print "Who is the Author?"
+   author <- getLine
+   print (createEmail recipient title author)
