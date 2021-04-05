61abf66828d243b1f873023c76dd391649b89737
Hammy <sgoudham@gmail.com>
2021-04-05 01:17:06 +0100

    Update names list

--- Unit 1/Lesson 4/AllNames.hs
+++ Unit 1/Lesson 4/AllNames.hs
@@ -3,6 +3,7 @@ module AllNames where
 names :: [([Char], [Char])]
 names = [("Callum", "Kilmartin"),
          ("Goudham","Suresh"),
+         ("Matt", "Hardman"),
          ("Theo", "Hardman"),
          ("Luca","Valentini"),
          ("Cormac", "Muir")]
